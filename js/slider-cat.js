3a723a25887d32236996da4e040b7fd73230af31
Artem Pupkov <49019709+DiamondKirov@users.noreply.github.com>
2019-10-31 12:38:39 +0300

    Update slider-cat.js

--- js/slider-cat.js
+++ js/slider-cat.js
@@ -28,8 +28,8 @@ $(document).ready(function () {
     })
     
     $(document).bind('scroll', function(){
-        console.log($(document).scrollTop)
-        if ($(document).scrollTop > 200){
+        console.log($(document).scrollTop())
+        if ($(document).scrollTop() > 200){
 
             $('.categories__item').css({
               transfrom: 'scale(1)'  

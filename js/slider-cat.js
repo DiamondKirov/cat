070af70327ca96e6900dafbb29ed59e53ab36bc0
Artem Pupkov <49019709+DiamondKirov@users.noreply.github.com>
2019-10-31 12:28:38 +0300

    Update slider-cat.js

--- js/slider-cat.js
+++ js/slider-cat.js
@@ -26,6 +26,13 @@ $(document).ready(function () {
             })
         }
     })
+    
+    if ($(document).scrollTop > 200){
+        
+        $('.categories__item').css({
+          transfrom: 'scale(1)'  
+        })
+    }
 
    
 
